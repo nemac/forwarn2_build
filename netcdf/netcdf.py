1da68eb6026ca1b9d64fed29844036e04817fa43
Jeff Bliss <jeff.a.bliss@gmail.com>
2019-12-17 15:35:47 -0500

    Can currently download all 26 terra and aqua tiles and gunzip them.

--- netcdf/netcdf.py
+++ netcdf/netcdf.py
@@ -1,5 +1,5 @@
 from braceexpand import braceexpand
-import wget
+import os, wget, gzip, shutil
 
 def get_terra_and_aqua_list(type, year, dayofyear):
   finalList = []
@@ -21,10 +21,22 @@ def get_terra_and_aqua_list(type, year, dayofyear):
   return finalList
 
 def download_and_extract(type, year, dayofyear):
+  listOfGzipFiles = []
+  destinationDirectory = './netcdf_test_dir/'
   list = get_terra_and_aqua_list(type, year, dayofyear)
-  print(len(list))
+  wget.download('https://gimms.gsfc.nasa.gov/MODIS/std/GMOD09Q1/tif/NDVI/2019/001/GMOD09Q1.A2019001.08d.latlon.x00y09.6v1.NDVI.tif.gz', './netcdf_test_dir') # this is a test wget
   #for i in list:
-    #wget.download(i, './netcdf_test_dir')
+  #  wget.download(i, destinationDirectory)
+  for i in (os.listdir(destinationDirectory)): # find all the files we just downloaded previously
+    listOfGzipFiles.append(i)
+  for i in listOfGzipFiles:
+    tifFile = i.strip('.gz')
+    with gzip.open(destinationDirectory + i, 'r') as f_in, open(destinationDirectory + tifFile, 'wb') as f_out: #open('./netcdf_test_dir/{}'.format(i).strip('.gz'), 'wb') as f_out:
+      shutil.copyfileobj(f_in, f_out)
+    os.remove(destinationDirectory + i)
+
+#def merge_terra_and_aqua_tif():
+#  os.system('python3 gdal_merge')
 
 def main():
   download_and_extract('std', '2019', '001')
